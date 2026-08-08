0fb743cf1c1a6ec762cf2a33a46ee3f2624cd3a9
24sathvik <devanapallisathvik@gmail.com>
2026-08-08 16:23:49 +0530

    feat: initialize TinaCMS configuration with blog, news, and gallery schemas and update build scripts

--- tina/config.ts
+++ tina/config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
-  branch: "main",
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID!,
-  token: process.env.TINA_TOKEN!,
+  branch:
+    process.env.GITHUB_BRANCH ||
+    process.env.VERCEL_GIT_COMMIT_REF ||
+    process.env.HEAD ||
+    "main",
+  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID || "",
+  token: process.env.TINA_TOKEN || "",
 
   build: {
     outputFolder: "admin",
